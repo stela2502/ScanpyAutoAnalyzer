477c0769a26b035cdefe5e44ba1ea0fb5102a61c
Stefan Lang <Stefan.Lang@med.lu.se>
2021-09-14 10:25:40 +0200

    And with all the new stuff.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ScanpyAutoAnalyzer-stela2502",
+    version="0.0.1",
+    author="Stefan Lang",
+    author_email="Stefan.Lang@med.lu.se",
+    description="P simple tool to prepare and run prepared jupyter notebooks.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/stela2502/ScanpyAutoAnalyzer",
+    project_urls={
+        "Bug Tracker": "https://github.com/stela2502/ScanpyAutoAnalyzer/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
