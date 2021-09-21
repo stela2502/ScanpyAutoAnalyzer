d99c820dd12a24ca176ba67045b9af9986f7a331
Stefan Lang <Stefan.Lang@med.lu.se>
2021-09-21 14:32:50 +0200

    I can now test the script - but it fails...

---
+++ src/ScanpyAutoAnalyzer/data/req.py
@@ -0,0 +1,14 @@
+import scvelo as scv
+import loompy
+import scanpy
+import igraph
+import phate
+import glob, os
+import pandas as pd
+import os
+import re
+import subprocess
+from collections import Counter
+import numpy as np
+from shutil import rmtree
+import jupytext
