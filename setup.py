bdd705dfd026dfe6dde82bd424d9e6728c3b51c7
Stefan Lang <Stefan.Lang@med.lu.se>
2022-01-14 11:24:40 +0100

    version to 0.02 to check if it updates.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ScanpyAutoAnalyzer-stela2502",
-    version="0.0.1",
+    version="0.0.2",
     author="Stefan Lang",
     author_email="Stefan.Lang@med.lu.se",
     description="A simple tool to prepare and run prepared jupyter notebooks.",
