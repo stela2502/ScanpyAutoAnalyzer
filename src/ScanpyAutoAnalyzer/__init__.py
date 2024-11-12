2999823f6419f2103def8fbc8f403f8765350cea
Stefan Lang <Stefan.Lang@med.lu.se>
2024-11-12 14:13:32 +0100

    adding function to the export.

--- src/ScanpyAutoAnalyzer/__init__.py
+++ src/ScanpyAutoAnalyzer/__init__.py
@@ -16,3 +16,21 @@ import anndata
 from .data import help
 
 print( help() )
+__all__ = [
+	"copy_script_to_path",
+    "getScript4",
+    "help",
+    "read_first_non_code_section",
+    "addSampleDataRust",
+    "testRP",
+    "dropRP",
+    "testMT",
+    "dropMT",
+    "testGene",
+    "write_top_genes_per_cluster",
+    "write_stats_tables",
+    "remove_gzipped_files",
+    "gzip_files_in_directory",
+    "write_data_to_directory",
+    "impute_expression_data",
+]
